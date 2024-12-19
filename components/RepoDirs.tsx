58c5301b0e8ad5377da3e23bc4698b1e7fd30954
jeeyoung <mbmtms@naver.com>
2024-12-19 12:32:05 +0900

    1st

---
+++ components/RepoDirs.tsx
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+
+interface RepoProps {
+  name: string
+}
+
+const RepoDirs: React.FC<RepoProps> = async ({ name }) => {
+  const username = 'bradtraversy'
+  const response = await fetch(
+    `https://api.github.com/repos/${username}/${name}/contents`
+  )
+  await new Promise((resolve) => setTimeout(resolve, 1000))
+  const contents = await response.json()
+  const dirs = contents.filter((content: any) => content.type === 'dir')
+  //console.log(contents)
+  //console.log(dirs)
+
+  return (
+    <div className="mt-2">
+      <h3 className="text-xl font-bold">Directoires</h3>
+      <ul>
+        {dirs.map((dir: any) => (
+          <li key={dir.path}>
+            <Link
+              className="underline"
+              href={`https://github.com/${username}/${name}/tree/master/${dir.path}`}
+            >
+              {dir.path}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
+export default RepoDirs
