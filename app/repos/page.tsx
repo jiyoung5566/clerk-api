a7173eae7a1c02048d1346258b6c68fe98b6d91c
jeeyoung <mbmtms@naver.com>
2024-12-19 12:37:13 +0900

    1st

--- app/repos/page.tsx
+++ app/repos/page.tsx
@@ -4,16 +4,16 @@ import { FaCodeBranch, FaEye, FaStar } from 'react-icons/fa'
 import { username } from '../constants'
 
 export default async function ReposPage() {
-  //1.SSG: Static site generation 정적페이지 생성
-  //const response = await fetch(`https://api.github.com/users/${username}/repos`)
+  // 1. SSG: Static site generation 정적페이지 생성
+  // const response = await fetch(`https://api.github.com/users/${username}/repos`)
 
-  //2. SSR: server-side rendering 동적페이지 생성
-  // const reponse = await fetch(
-  //  `https://api.github.com/users/${username}/repos`,
-  //  { cache: `no-store` }
+  // 2. SSR: server-side rendering 동적페이지 생성
+  // const response = await fetch(
+  //   `https://api.github.com/users/${username}/repos`,
+  //   { cache: 'no-store' }
   // )
 
-  //3.ISR: incremental static generation 일정 시간 간격으로 페이지 갱신
+  // 3. ISR: incremental static generation 일정 시간 간격으로 페이지 갱신
   const response = await fetch(
     `https://api.github.com/users/${username}/repos`,
     { next: { revalidate: 60 } }
@@ -28,25 +28,28 @@ export default async function ReposPage() {
         Github repositories of {username}
       </h2>
       <ul>
-        {repos.map((repo: any) => (
-          <li key={repo.id} className="bg-gray-600 m-4 p-4 rounded-md">
-            <Link href={`/repos/${repo.name}`}>
-              <h3 className="text-xl font-bold"> {repo.name} </h3>
-              <p>{repo.description} </p>
-              <div className="flex items-center justify-between">
-                <span className="flex items-center gap-1">
-                  <FaStar /> {repo.stargazers_count}
-                </span>
-                <span className="flex items-center gap-1">
-                  <FaCodeBranch /> {repo.forks_count}
-                </span>
-                <span className="flex items-center gap-1">
-                  <FaEye /> {repo.watchers_count}
-                </span>
-              </div>
-            </Link>
-          </li>
-        ))}
+        {
+          /* eslint-disable @typescript-eslint/no-explicit-any */
+          repos.map((repo: any) => (
+            <li key={repo.id} className="bg-gray-100 m-4 p-4 rounded-md">
+              <Link href={`/repos/${repo.name}`}>
+                <h3 className="text-xl font-bold"> {repo.name} </h3>
+                <p> {repo.description} </p>
+                <div className="flex items-center justify-between">
+                  <span className="flex items-center gap-1">
+                    <FaStar /> {repo.stargazers_count}
+                  </span>
+                  <span className="flex items-center gap-1">
+                    <FaCodeBranch /> {repo.forks_count}
+                  </span>
+                  <span className="flex items-center gap-1">
+                    <FaEye /> {repo.watchers_count}
+                  </span>
+                </div>
+              </Link>
+            </li>
+          ))
+        }
       </ul>
     </div>
   )
--- components/RepoDirs.tsx
+++ components/RepoDirs.tsx
@@ -1,3 +1,4 @@
+import { username } from '@/app/constants'
 import Link from 'next/link'
 
 interface RepoProps {
@@ -5,32 +6,34 @@ interface RepoProps {
 }
 
 const RepoDirs: React.FC<RepoProps> = async ({ name }) => {
-  const username = 'bradtraversy'
   const response = await fetch(
     `https://api.github.com/repos/${username}/${name}/contents`
   )
   await new Promise((resolve) => setTimeout(resolve, 1000))
   const contents = await response.json()
+  /* eslint-disable @typescript-eslint/no-explicit-any */
   const dirs = contents.filter((content: any) => content.type === 'dir')
-  //console.log(contents)
-  //console.log(dirs)
 
   return (
     <div className="mt-2">
-      <h3 className="text-xl font-bold">Directoires</h3>
+      <h3 className="text-xl font-bold">Directories</h3>
       <ul>
-        {dirs.map((dir: any) => (
-          <li key={dir.path}>
-            <Link
-              className="underline"
-              href={`https://github.com/${username}/${name}/tree/master/${dir.path}`}
-            >
-              {dir.path}
-            </Link>
-          </li>
-        ))}
+        {
+          /* eslint-disable @typescript-eslint/no-explicit-any */
+          dirs.map((dir: any) => (
+            <li key={dir.path}>
+              <Link
+                className="underline"
+                href={`https://github.com/${username}/${name}/tree/master/${dir.path}`}
+              >
+                {dir.path}
+              </Link>
+            </li>
+          ))
+        }
       </ul>
     </div>
   )
 }
+
 export default RepoDirs
